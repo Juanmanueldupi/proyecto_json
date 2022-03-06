a5888bfbaa51bfa0db3cf9b2b623944011931d9d
juanmanueldupi <juanmadupi@gmail.com>
2022-03-06 22:52:27 +0100

    Funciones para usar en el programa principal FINALIZADO

--- funciones.py
+++ funciones.py
@@ -11,17 +11,42 @@ def leer_json(fichero):
         print("Error al leer el fichero")
         sys.exit(0)
 
+
+#Menu
+
+def MostrarMenu():
+    menu='''
+    1. Lista el nombre de los animes.
+    2. Contar todos los estudios. 
+    3. Pide por teclado un anime y muestra el estudio que lo realiza.
+    4. Pedir por teclado un genero y mostrar los animes que pertenecen a el.(Action,Mystery,Seinen,Supernatural,Comedy,Ecchi,Fantasy,Harem,Romance,School,Sci-Fi,Super Power,Drama,Military,Police,Magic,Adventure,Slice of Life,Demons,Mecha,Horror,Space,Ai)
+    5. Pide por teclado un estudio y te muestra los animes realizados(si hay mas de uno, muestra todos) y su descripcion.
+    6. Salir
+    '''
+    print(menu)
+    while True:
+        try:
+            opcion=int(input("Opción:"))
+            return opcion
+        except:
+            print("Opción incorrecta, debe ser un número")
+
+# 1. Lista el nombre de los animes.
+
 def listar_animes(datos):
     lista=[]
     for anime in datos:
         lista.append(anime.get("title").get("text"))
     return lista
 
+# 2. Contar todos los estudios. 
+
 def listar_estudios(datos):
     lista=[]
     for anime in datos:
         lista.append(anime.get("studio"))
     return lista
+
 
 def contar_estudios(datos):
     lista=[]
@@ -39,6 +64,12 @@ def mostrar_estudios(datos,estudios):
             return anime.get("studio")
 
 #4. Pedir por teclado un genero y mostrar los animes que pertenecen a el.
+
+#def listar_generos(datos):
+#    lista=[]
+#    for genero in datos:
+#        lista.append(genero.get("genres"))
+#    return genero
 
 def mostrar_animes(datos,genero):
     lista=[]
